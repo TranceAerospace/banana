0cfb2cd43359056a4a4488f8d28e643e6a6f273d
Trance <xp46290@gmail.com>
2023-01-17 22:54:17 -0500

    these suck

--- Bucketlist/ContentView.swift
+++ Bucketlist/ContentView.swift
@@ -8,19 +8,83 @@
 import SwiftUI
 
 struct ContentView: View {
+    let users = [
+    User(firstName: "Arnold", lastName: "Rimmer"),
+    User(firstName: "Kristine", lastName: "Kochanski"),
+    User(firstName: "David", lastName: "Lister")
+    ].sorted()
+
+    var loadingState = LoadingState.loading
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        if loadingState == .loading {
+            LoadingView()
+        } else if loadingState == .success {
+            SuccessView()
+        } else if loadingState == .failed {
+            FailedView()
         }
-        .padding()
+        
+        Text("Hello World")
+            .onTapGesture {
+                let str = "Test Message"
+                let url = getDocumentsDirectory().appendingPathComponent("message.txt")
+                
+                do {
+                    try str.write(to: url, atomically: true, encoding: .utf8)
+                    let input = try String(contentsOf: url)
+                    print(input)
+                } catch {
+                    print(error.localizedDescription)
+                }
+            }
+    }
+    
+    func getDocumentsDirectory() -> URL {
+        // find all possible documents directories for this user
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        
+        // just send back the first one, which ought to be the only one
+        return paths[0]
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct User: Identifiable, Comparable {
+    let id = UUID()
+    let firstName: String
+    let lastName: String
+    
+    static func < (lhs: User, rhs: User) -> Bool {
+        return lhs.lastName < rhs.lastName
+    }
+    
+}
+
+enum LoadingState {
+    case loading, success, failed
+}
+
+struct LoadingView: View {
+    var body: some View {
+        Text("Loading...")
+    }
+}
+
+struct SuccessView: View {
+    var body: some View {
+        Text("Success!")
+    }
+}
+
+struct FailedView: View {
+    var body: some View {
+        Text("Failed...")
     }
 }
